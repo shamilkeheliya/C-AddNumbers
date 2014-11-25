2a2678f0740929d2cf22ea929c0e2458cf044231
randy808 <randyn52689@gmail.com>
2014-11-24 20:29:14 -0500

    Corrected a mistake in scanf statement

--- addNumbers.c
+++ addNumbers.c
@@ -6,10 +6,12 @@ int main()
 	int enteredNum = 0;
 	int sum = 0;
 
-	while( scanf("Enter a number you would like to be added to the sum : %d", &enteredNum) != EOF)
+	printf("Enter a number you would like to be added to the sum : ");
+	
+	while( scanf("%d", &enteredNum) != EOF)
 	{
 		sum+=enteredNum;
-
+		printf("Enter a number you would like to be added to the sum : ");
 	}
 
 	printf("The sum of the numbers you entered is %d\n",sum);
