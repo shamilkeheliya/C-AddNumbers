c28db41c32b89e1807cbe60fd1376f3467801db6
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2020-10-01 23:08:55 +0530

    New way to get sum

--- addNumbers.c
+++ addNumbers.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-
-
 int main()
 {
-	int a,b,c,total;
-	printf("enter values");
-	scanf("%d %d %d",& a ,& b ,& c);
-	total = a+b+c;
-	printf("%d",total);
+	int num,total;
+
+	do
+    {
+        if(num!=-999)
+        {
+            printf("Enter number for add or enter -999 to get sum\n");
+            scanf("%d",&num);
+            total=total+num;
+        }
+    }while(num!=-999);
+
+    printf("Total = %d",total+999);
+
 	return 0;
-
-
 }
